bddf86db7177bc25510db72bc5388caab4d29b4d
Alex Rogers <alex.rogers823@gmail.com>
2025-02-07 15:40:59 -0800

    generated components for debts, items, and savings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DebtsComponent } from './debts/debts.component';
+import { ItemsComponent } from './items/items.component';
+import { SavingsComponent } from './savings/savings.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DebtsComponent,
+    SavingsComponent,
+    ItemsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
